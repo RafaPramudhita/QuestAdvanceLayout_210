43419515d9c4d8437193e595ebf87fc6ad41035e
RafaPramudhita <rafapram360@gmail.com>
2025-10-16 13:09:06 +0700

    4. composable Box (full size + center)

--- app/src/main/java/com/example/pertemuan3/TataLetak.kt
+++ app/src/main/java/com/example/pertemuan3/TataLetak.kt
@@ -47,6 +47,21 @@ fun TataLetakRow(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataLetakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Column 2")
+    }
+}
+
 
 
 
