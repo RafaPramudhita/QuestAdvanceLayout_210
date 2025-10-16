3b4dba689f76e359fa79c35a3f53be2eb2c65e77
RafaPramudhita <rafapram360@gmail.com>
2025-10-16 13:11:04 +0700

    5. layout campuran ColumnRow (Baris 1)

--- app/src/main/java/com/example/pertemuan3/TataLetak.kt
+++ app/src/main/java/com/example/pertemuan3/TataLetak.kt
@@ -62,6 +62,21 @@ fun TataLetakBox(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataLetakColumnRow(modifier: Modifier) {
+    Column {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+
+    }
+}
+
 
 
 
